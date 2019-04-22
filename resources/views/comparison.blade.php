e7c43ad846ab3f88178dbb936a41a4f6fd074e3e
Aurang <aurangzebbbk@gmail.com>
2019-04-22 15:58:32 +0500

    intial commit

--- resources/views/comparison.blade.php
+++ resources/views/comparison.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 4 meta tags *Must* come first in the head; any other head content must come *after* these tags -->
 
-    <!-- Title -->
+    
     <title>Clever - Education &amp; Courses Template | Blog</title>
 
     <!-- Favicon -->
