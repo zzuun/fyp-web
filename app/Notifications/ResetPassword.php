d2bb9919c1f2f8013b3edb301be22ab48d8f0eee
Ameer Hamza <xovolt.hamza@gmail.com>
2019-04-24 04:41:36 +0500

    heroku 12

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    use Queueable;
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+      return (new MailMessage)
+      ->from(env('MAIL_USERNAME'),'KnSaCareer')
+      ->subject(Lang::getFromJson('Reset Password Notification'))
+      ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your Admin account.'))
+      ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('admin.password.reset', ['token' => $this->token], false)))
+      ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.admins.expire')]))
+      ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
