456497e6ae9067bb26653a4ce4109b0cf3e91909
Ameer Hamza <xovolt.hamza@gmail.com>
2019-04-24 04:11:46 +0500

    heroku 10

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost:8000'),
+    'url' => env('APP_URL', 'https://knsacareer.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
