9b413d4cde2cf33c6d90d98d899b256d1a797c85
Ameer Hamza <xovolt.hamza@gmail.com>
2019-04-25 19:08:17 +0500

    college

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -94,6 +94,7 @@ class MainController extends Controller
       $result = DB::table('degrees')
       ->join('institutes','institutes.id','degrees.institute_id')
       ->where('degrees.id',$degreeId)
+      ->where('instituteType','College')
       ->join('addresses','addresses.institute_id','institutes.id')
       ->select('degrees.name as degreeName','degrees.duration','degrees.system','institutes.name as instituteName',
       'institutes.id as instituteID','degrees.noOfSeats','degrees.creditHours','degrees.lastMerit',
@@ -102,6 +103,7 @@ class MainController extends Controller
       $inc = DB::table('degrees')->where('degrees.id',$degreeId)->increment('numberOfViews');
       $more =  DB::table('degrees')
       ->join('institutes','institutes.id','degrees.institute_id')
+      ->where('instituteType','College')
       ->where('institutes.id',$result[0]->instituteID)
       ->select('degrees.name as degreeName','institutes.name as instituteName','degrees.id as degreeid','institutes.id as instituteid')
       ->take(3)
@@ -340,7 +342,9 @@ class MainController extends Controller
       $more =  DB::table('degrees')
       ->join('institutes','institutes.id','degrees.institute_id')
       ->join('degreeGroups','degreeGroups.id','degrees.degree_groups_id')
+      ->join('departments','degrees.department_id','departments.id')
       ->where('degreeGroups.id',$result[0]->type)
+      ->where('instituteType','University')
       ->where('institutes.id','!=',$result[0]->instituteID)
       ->whereNotIn('degreeLevel',['INTER','MS'])
       ->select('degrees.name as degreeName','institutes.name as instituteName',
