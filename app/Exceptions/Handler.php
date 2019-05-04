e713454cc7df1ccb5a8dbace1dab1606b68740a8
Ameer Hamza <xovolt.hamza@gmail.com>
2019-05-05 02:38:45 +0500

    change

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,7 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+// use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,10 +48,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof MethodNotAllowedHttpException)
-        {
-          return back()->with('success', 'Get Method is not allowed for the requested route');
-        }
         return parent::render($request, $exception);
     }
 
